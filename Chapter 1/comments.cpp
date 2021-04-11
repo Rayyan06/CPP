654cbb48964c89e4672053fe8d0bab9fe34813bc
Rayyan Khan <weatherwhiz06@gmail.com>
2021-04-11 07:49:02 -0400

    1.1 and 1.2

---
+++ Chapter 1/comments.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!"; // This is a comment in line
+    std::cout << "Hello world\n"; // std::cout is part of iostream
+
+    
+    // std::cout lives in the iostream library
+    std::cout << "Hello world!\n";
+    
+    // this is much easier to read
+    std::cout << "It is very nice to meet you!\n";
+    
+    // don't you think so?
+    std::cout << "Yeah!\n";
+
+    /*
+     * This is a multiline comment
+     */
+    return 0;
+}
---
+++ Chapter 1/variables.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    return 0;
+}
